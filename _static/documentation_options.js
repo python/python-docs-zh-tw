fe2adf0d9cfa8e654dc7470396307682ac4dd299
mattwang44 <mattwang44@gmail.com>
2023-08-18 18:39:39 +0000

    Deploying to gh-pages from  @ bbc8024ce34ee8b352d10f52d17d6d7e429daf84 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,4 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+const DOCUMENTATION_OPTIONS = {
     VERSION: '3.12.0rc1',
     LANGUAGE: 'zh-TW',
     COLLAPSE_INDEX: false,
