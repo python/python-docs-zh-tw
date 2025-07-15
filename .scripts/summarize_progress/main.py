c71602e8af4b4ba1826fb3246c28684062d624f5
W. H. Wang <mattwang44@gmail.com>
2025-07-15 17:54:59 +0800

    fix: issue display in summary table

--- .scripts/summarize_progress/main.py
+++ .scripts/summarize_progress/main.py
@@ -163,7 +163,7 @@ if __name__ == "__main__":
             po_link = f"https://github.com/python/python-docs-zh-tw/tree/3.13/{file_path}"
             issue_link = filedata['issue']
             issue_number = f"#{issue_link.split('/')[-1]}"
-            create_issue_link = f"https://github.com/python/python-docs-zh-tw/issues/new?title=Translate `{file_path}`"
+            create_issue_link = f"https://github.com/python/python-docs-zh-tw/issues/new?title=Translate%20`{file_path}`"
             issue_display = format_line_po_issue_display(issue_link, issue_number, filedata['progress'], create_issue_link)
             line_po = format_line_po(filename, po_link, filedata['progress'], issue_display, filedata['assignee'])
             writeliner.append(line_po)
